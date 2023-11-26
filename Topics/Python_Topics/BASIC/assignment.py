c7d11947389516b5932f3736beb692bd493bdcb8
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-26 13:39:00 +0530

    ...

---
+++ Topics/Python_Topics/BASIC/assignment.py
@@ -0,0 +1,16 @@
+#Assignment operators
+
+'''
+    = : to assign value
+
+    a=30
+
+    += : a+=10 ==> a = a+10
+        
+        a = 30+10 = 40
+
+
+    -= : a-=20 : a = 10
+
+    *= : a*=10 : a=300
+'''
