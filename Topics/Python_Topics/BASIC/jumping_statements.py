24a7a99fd60003a90f8eff8a1afd469a18a8672d
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-26 13:41:29 +0530

    ...

---
+++ Topics/Python_Topics/BASIC/jumping_statements.py
@@ -0,0 +1,16 @@
+'''
+    jumping statements 
+    
+    break: will break the iteration 
+
+    continue : will skip the iteration and continue from next
+
+    pass: to do nothing
+'''
+
+for i in range(10):
+    if i==6:
+        break
+
+    print(i)
+    
