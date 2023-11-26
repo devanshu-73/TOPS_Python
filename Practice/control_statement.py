6b785c9e36906ab4b35c62c226556fa2142aeb5a
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-26 16:23:16 +0530

    ...

---
+++ Practice/control_statement.py
@@ -0,0 +1,32 @@
+n1 = int(input("Enter N1 : "))
+
+if(n1>=95):
+    print("A grade.. GOAT")
+elif(n1>=70 and n1<95):
+    print("B grade.. Wow")
+elif(n1>=50 and n1<70):
+    print("C grade.. Nice")
+elif(n1>=33 and n1<50):
+    print("D grade.. ok ok")
+else:
+    print("Fail...")
+
+
+
+'''
+n1 = int(input("Enter N1 : "))
+
+if(n1 >= 33):
+    if(n1>=50):
+        if(n1>=70):
+            if(n1>=95):
+                    print("A grade... Wow...")  
+            else:
+                    print("B grade... excellent..")
+        else:
+            print("C grade.. Nice..")
+    else:
+        print("D grade... Good..")
+else:
+    print("Fail...")
+'''  
