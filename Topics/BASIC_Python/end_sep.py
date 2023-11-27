88b5f65932c14111eb363e39e851c87bba624e5f
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-27 19:18:49 +0530

    ...

--- Topics/BASIC_Python/end_sep.py
+++ Topics/BASIC_Python/end_sep.py
@@ -1,11 +1,10 @@
+
 age=33
 name="abc"
 
 print("Your age is : ",age)
 print("Your Name is : "+name) # string concate
-
 print(f'Name is {name} and Age is {age}')
-
 print("Name is {1} and Age is {0}".format(age,name))
 
 
@@ -14,6 +13,7 @@ print("hello",end="$")
 print("world")
 
 # sep
+
 s1="Hello"
 s2="World"
 print(s1,s2,sep="-")
