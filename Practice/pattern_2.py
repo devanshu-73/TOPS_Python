563ac87df699770e50b90e261ed159b51208a731
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-28 15:25:48 +0530

    ....

--- Practice/pattern_2.py
+++ Practice/pattern_2.py
@@ -10,8 +10,8 @@
 
 # n = int(input("Enter Number : "))
 
-# for i in range(n):
-#     for j in range(n):
+# for i in range(5):
+#     for j in range(5):
 #         if(j<=i):
 #             if ((i+j)%2 == 0):
 #                 print(1,end=" ")
@@ -19,11 +19,28 @@
 #                 print(0,end=" ") 
 #     print()
 
-
-for i in range(1,6):
-    for j in range(i):
-        if (i + j) % 2 == 0:
-            print("1", end=" ")
-        else:
-            print("0", end=" ")
+for i in range(5):
+    for j in range(i + 1):
+        print(1 if (i + j) % 2 == 0 else 0, end=" ")
     print()
+
+# for i in range(1, 6):
+#     for j in range(i):
+#         if j % 2 == 0:
+#             print("1", end=" ")
+#         else:
+#             print("0", end=" ")
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,i):
+#         if (i + j) % 2 == 0:
+#             print("0", end=" ")
+#         else:
+#             print("1", end=" ")
+#     print()
+
+
+# for i in range(1,6):
+#     row_values = " ".join("0" if (i + j) % 2 == 0 else "1" for j in range(i))
+#     print(row_values)
