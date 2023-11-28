d4b8f6725375f43f10671ebd8388445f92c53409
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-28 15:47:21 +0530

    ...

--- Practice/pattern_2.py
+++ Practice/pattern_2.py
@@ -25,23 +25,14 @@
 #         print(1 if (i + j) % 2 == 0 else 0, end=" ")
 #     print()
 
-
-# for i in range(1, 6):
-#     for j in range(i):
-#         if j % 2 == 0:
-#             print("1", end=" ")
-#         else:
-#             print("0", end=" ")
-#     print()
-
-# for i in range(1,6):
-#     for j in range(1,i):
-#         if (i + j) % 2 == 0:
-#             print("0", end=" ")
-#         else:
-#             print("1", end=" ")
-#     print()
-
+for i in range(5):
+    for j in range(i+1):
+        if((i+j)%2 == 0):
+            print("1",end=" ")
+        else:
+            print("0",end=" ")
+    print()
+    
 
 # for i in range(1,6):
 #     row_values = " ".join("0" if (i + j) % 2 == 0 else "1" for j in range(i))
