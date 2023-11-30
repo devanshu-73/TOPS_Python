71461053993e71ea1d50fb80cd2ccc5a3de33eb0
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-30 17:48:23 +0530

    ....

---
+++ Topics/BASIC_Python/08-loops.py
@@ -0,0 +1,28 @@
+# loops: for, while and nested loop
+
+# For Loop
+# for i in range(start,stop,step):
+#     code...
+
+# While Loop
+# while(condition):
+#     code...
+
+
+# nested loops
+
+# for i in range(start,stop,step):
+#     for j in range(start,stop,step):
+#         code.....
+
+# for i in range(start,stop,step):
+#     while(condi..):
+#         code.....
+
+# while():
+#     while():
+#         code.....
+
+# while():
+#     for i in range(start,stop,step):
+#         code.....
--- Topics/BASIC_Python/second.py
+++
@@ -1,4 +0,0 @@
-print("Mom's  Birthday")
-print("""Mom's \n Birthday""")
-print('Mom\'s Birthday')
-print("I'm an \"INDIAN\" ")
