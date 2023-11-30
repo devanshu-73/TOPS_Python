210a0fe91e37fcdff3f5e7d606931ece8d8d0de4
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-30 17:41:38 +0530

    ....

---
+++ Topics/BASIC_Python/00-jumping_statement.py
@@ -0,0 +1,11 @@
+# Jumping statements in python
+
+# break : will stop loop
+# pass : when block of code syntatically needed but nno need to create any logic
+# continue : will skip the iteration
+
+for i in range(10):
+    if i==5:
+        pass
+
+    print(f"{i} Hello")
