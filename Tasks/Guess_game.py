09db68d6e04a1003b81ef6b915cf90cbea27c0a3
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-30 17:41:08 +0530

    ....

--- Tasks/Guess_game.py
+++ Tasks/Guess_game.py
@@ -9,21 +9,19 @@ while(status):
     chance = 5
     user = int(input("Enter your Num : "))
 
-    while(user):
-            if (chance<=5):
-                if (user>lucky):
-                    print("Too Large")
-                    print(chance)
-                    chance=chance-1
-                    user = int(input("Enter your Num : "))
-                elif (user<lucky):
-                    print("too small")
-                    print(chance)
-                    chance=chance-1
-                    user = int(input("Enter your Num : "))
-                elif (user == lucky):
-                    print("got it")
-                    break
+    while (chance>0):
+        if (user>lucky):
+            print("Too Large")
+            user = int(input("Enter your Num : "))
+        elif (user<lucky):
+            print("too small")
+            user = int(input("Enter your Num : "))
+        elif (user == lucky):
+            print("got it")
+            break
+        
+        chance=chance-1
+        print("chances : ",chance)
         
     user_intr=input("Do u wanna play again...? [y/n]")
     if(user_intr == "y" or user_intr == "Y"):
