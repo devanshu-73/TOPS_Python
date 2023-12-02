520229d885db7e27aef0c7e182313b3a2758e8ad
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-02 18:37:06 +0530

    ....

--- Tasks/task_list.py
+++ Tasks/task_list.py
@@ -5,7 +5,7 @@
 
 # task-1 :
 l1 = []
-n = int(input("enter n : "))
+n = int(input("enter element : "))
 for i in range(n):
     num = int(input(f"enter number {i+1} : "))
     l1.append(num)
@@ -13,8 +13,12 @@ for i in range(n):
 # even_list = []
 even=0 
 odd=0 
-for j in range(10):
+for j in range(n):
     if l1[j]%2 == 0 :
-        even
+        even=even+1
+    else:
+        odd=odd+1
+print("even : ",even)
+print("odd : ",odd)
 # for i in range(10):
 #     print(f"num {i} :",l1[i])
