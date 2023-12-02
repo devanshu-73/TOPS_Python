9a0d2ab7c85c0ad37c1c295ada516705186f5531
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-02 19:21:06 +0530

    ....

--- Tasks/task_list.py
+++ Tasks/task_list.py
@@ -1,24 +1,26 @@
 # list Task-1 ; take 10 input and odd even counting
 
-# list task-2 : reverse the string without sort() method sort (reverse = True)
-
 
 # task-1 :
-l1 = []
-n = int(input("enter element : "))
-for i in range(n):
-    num = int(input(f"enter number {i+1} : "))
-    l1.append(num)
+# l1 = []
+# n = int(input("enter element : "))
+# for i in range(n):
+#     num = int(input(f"enter number {i+1} : "))
+#     l1.append(num)
     
 # even_list = []
-even=0 
-odd=0 
-for j in range(n):
-    if l1[j]%2 == 0 :
-        even=even+1
-    else:
-        odd=odd+1
-print("even : ",even)
-print("odd : ",odd)
-# for i in range(10):
-#     print(f"num {i} :",l1[i])
+# odd_list = []
+# even=0 
+# odd=0 
+# for j in range(n):
+#     if l1[j]%2 == 0 :
+#         even_list.append(l1[j])
+#     else:
+#         odd_list.append(l1[j])
+# print("even : ",even_list)
+# print("odd : ",odd_list)
+
+# list task-2 : reverse the string without sort() method sort (reverse = True)
+
+str1 = "hello world"
+str2 = str1[::-1]
