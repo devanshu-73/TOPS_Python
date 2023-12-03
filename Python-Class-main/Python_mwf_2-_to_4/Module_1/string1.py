40073c09f3ebf5240f4bc6547579fa5876a43bcf
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-03 20:40:52 +0530

    ...

---
+++ Python-Class-main/Python_mwf_2-_to_4/Module_1/string1.py
@@ -0,0 +1,15 @@
+# String slicing
+
+str1="HEllo World"
+
+print(str1[-1:-12:-1])
+
+print(str1[3:9]) 
+
+print(str1[-2]) # -ve index
+
+print(str1[0:9:2]) # [start:stop:step] --> default step is 1
+
+# slice operator -> ':'
+
+#task : reverse a string w/o using inbuilt function
