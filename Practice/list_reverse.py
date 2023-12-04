2c574e52c1a313cf64988059fd81080bd76d1c44
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-04 14:45:06 +0530

    ....

--- Practice/list_reverse.py
+++ Practice/list_reverse.py
@@ -1,4 +1,4 @@
-l1 = [1,2,3,4,5,6,7,8]
+# l1 = [1,2,3,4,5,6,7,8]
 # l1.reverse()
 # print(l1)
 # l2=[]
@@ -6,3 +6,13 @@ l1 = [1,2,3,4,5,6,7,8]
 # l2 = l1[::-1]
 # print("l1 : ",l1)
 # print("l2 : ",l2)
+
+l1 = [1,2,3,4,5,6,7,8]
+start=0
+end=len(l1)-1
+for i in l1:
+    l1[start],l1[end]=l1[end],l1[start]
+    start = start+1
+    end = end-1
+
+print(l1)
