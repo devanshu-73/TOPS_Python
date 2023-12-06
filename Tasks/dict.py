85a714206fc63a076d027648a4d91b4954eeade8
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-06 18:00:33 +0530

    ....

---
+++ Tasks/dict.py
@@ -0,0 +1,14 @@
+# roll ,name,subject
+
+
+student = {}
+
+roll = int(input("Enter Roll: "))
+name = input("Enter name: ")
+subject = input("Enter Subject: ")
+
+student["roll"]=roll
+student["name"]=name
+student["subject"]=subject
+print(student)
+ 
