a6a44972fa22bbbfc44888699f9a754aaafef1b0
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-07 18:33:05 +0530

    Frequency_Task

--- Tasks/frequency_task.py
+++ Tasks/frequency_task.py
@@ -1,11 +1,23 @@
+# Frequency Task
+# code 1
+sentence = input("Enter Sentence : ")
+list1 = sentence.split(" ")
+print("LIST1 :",list1)
+dict1={}
+
+for item in list1:
+    count=0
+    for other_item in list1:
+        if item == other_item:
+            count=count+1
+    dict1[item]=count
+print("Dict1 :",dict1)
+
+
+# code 2
+from collections import Counter 
 sentence = input("Enter Sentence : ")
 
 list1 = sentence.split(" ")
-print("LIST1 :",list1)
-count=0
-dict1={}
-for i in range(len(list1)):
-    for item in list1[i:]:
-        count=count+1
-        dict1["item"]=count
+dict1 = Counter(list1)
 print("Dict1 :",dict1)
