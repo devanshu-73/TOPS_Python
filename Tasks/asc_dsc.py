ea99aafb3cc4ecf8769e6c7c920492a3f8a02186
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-08 16:40:44 +0530

    .....

--- Tasks/asc_dsc.py
+++ Tasks/asc_dsc.py
@@ -1,14 +1,14 @@
-# # ascending order
+# ascending order
 
-# li=[51,1,2,9,98,46,37,71]
+li=[51,1,2,9,98,46,37,71]
 
-# for i in range(len(li)):
-#     for j in range(i,len(li)):
-#         if(li[i]>li[j]):
-#             li[j]=li[i]+li[j] a = b+a
-#             li[i]=li[j]-li[i] b = a+b
-#             li[j]=li[j]-li[i] a = a+b
-# print(li)
+for i in range(len(li)):
+    for j in range(i,len(li)):
+        if(li[i]>li[j]):
+            li[j]=li[i]+li[j] a = b+a
+            li[i]=li[j]-li[i] b = a+b
+            li[j]=li[j]-li[i] a = a+b
+print(li)
         
 # # descending order
 
