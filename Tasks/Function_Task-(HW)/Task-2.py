3eed65db6a5bd975dd4ed34448b84766c08a3321
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-13 19:27:28 +0530

    ....

--- Tasks/Function_Task-(HW)/Task-2.py
+++ Tasks/Function_Task-(HW)/Task-2.py
@@ -2,12 +2,12 @@
 
 # ===========Choice Board===========
 
-# 	1. Addiiton
-# 	2. Substraction
-# 	3. Multiplication
-# 	4. Division
-# 	5. Modulo
-# 	6. Exit
+	# 1. Addiiton
+	# 2. Substraction
+	# 3. Multiplication
+	# 4. Division
+	# 5. Modulo
+	# 6. Exit
 
 # 	Enter Your Choice : 2
 	
@@ -24,9 +24,14 @@
 # 	repeat the process 
 
 
+print("===========Choice Board===========")
 
-
-
+print("1. Addiiton")
+print("2. Substraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Modulo")
+print("6. Exit")
 
 
 
