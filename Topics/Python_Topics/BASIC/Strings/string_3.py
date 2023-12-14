732e677be1d37ae8eceba8732175918934d3694d
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-14 18:47:12 +0530

    ....

--- Topics/Python_Topics/BASIC/Strings/string_3.py
+++
@@ -1,5 +0,0 @@
-
-str1="HEllo world Hope all are well"
-
-print(str1.split('l'))
-print(type(str1.split('l')))
--- Topics/Python_Topics/INTERMEDIATE/Tuple/ex1.py
+++
@@ -1,7 +0,0 @@
-t1=(12.43,34,54,6)
-
-for i in t1:
-    print(i)
-
-
-print("indexing : ",t1[2])
