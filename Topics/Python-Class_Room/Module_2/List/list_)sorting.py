e34117c2a448ae49dceb867ee207b852e13c1a16
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-15 16:37:06 +0530

    ...

--- Topics/Python-Class_Room/Module_2/List/list_)sorting.py
+++ Topics/Python-Class_Room/Module_2/List/list_)sorting.py
@@ -4,15 +4,11 @@ c=int(input("how many elements you want : ? "))
 
 for i in range(c):
     num=int(input("Enter element : "))
-
     l1.append(num)
 
-
-for i in range(len(l1)):
-    for j in  range(i+1,len(l1)):
-        if l1[i]<l1[j]:
-            l1[i],l1[j]=l1[j],l1[i]
-
-
+for i in range(len(l1)):              #i = 0
+    for j in  range(i+1,len(l1)):     #j = 1
+        if l1[i]>l1[j]:               #l1[0] = l1[1]   
+            l1[i],l1[j]=l1[j],l1[i]   #l1[0],l1[1] = l1[1],l1[0]
 
 print(l1)
