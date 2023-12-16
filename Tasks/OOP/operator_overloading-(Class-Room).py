536006b2dcc44b2ade6e85780772753cf7bed5fa
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-16 19:20:28 +0530

    operator_overloading

--- Tasks/OOP/operator_overloading-(Class-Room).py
+++ Tasks/OOP/operator_overloading-(Class-Room).py
@@ -1,3 +1,19 @@
+# solution 1 With Dunder/Magic Method
+
+class A:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def __eq__(self,value):
+        return(self.a == value.a and
+        self.b == value.b)
+a1 = A(12,4)
+a2 = A(12,4)
+
+print(a1==a2)
+
+# solution 2.1 Without Dunder/Magic Method
+
 # class A:
 #     def __init__(self,a,b):
 #         self.a = a
@@ -9,16 +25,19 @@
 # a2 = A(12,4)
 
 # print(a1.addition(a2))
-class A:
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-    def addition(self,value):
-        if(self.a == value.a and self.b == value.b):
-            return True
-        else:
-            return False
-a1 = A(12,4)
-a2 = A(12,4)
 
-print(a1.addition(a2))
+# solution 2.2 Without Dunder/Magic Method
+
+# class A:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#     def addition(self,value):
+#         if(self.a == value.a and self.b == value.b):
+#             return True
+#         else:
+#             return False
+# a1 = A(12,4)
+# a2 = A(12,4)
+
+# print(a1.addition(a2))
