66d65de075a2ba53c848dc5869cf4a8ea0563d1b
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-17 21:44:18 +0530

    operator_overloading

--- Tasks/OOP/operator_overloading-(Class-Room).py
+++ Tasks/OOP/operator_overloading-(Class-Room).py
@@ -1,16 +1,16 @@
 # solution 1 With Dunder/Magic Method
 
-class A:
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-    def __eq__(self,value):
-        return(self.a == value.a and
-        self.b == value.b)
-a1 = A(12,4)
-a2 = A(12,4)
+# class A:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#     def __eq__(self,value):
+#         return(self.a == value.a and
+#         self.b == value.b)
+# a1 = A(12,4)
+# a2 = A(12,4)
 
-print(a1==a2)
+# print(a1==a2)
 
 # solution 2.1 Without Dunder/Magic Method
 
