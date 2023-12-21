e7d3b72b31a5025e9504ac2d2dfed9394e55ea51
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-21 20:12:49 +0530

    Pizza_System

--- Tasks/OOP/Pizza_System.py
+++ Tasks/OOP/Pizza_System.py
@@ -8,54 +8,58 @@ class PizzaOrderSystem:
         self.total_bruschetta_given = 0
         self.total_chocco_brownies_given = 0
 
-    def display_menu(self):
-        print("1. large pizza = 100\n2. large Pizzas = 200\n3. Large Pizzas = 300")
-        print("***Buy 4 or more pizza and get 1.5lt of soft drink free***")
-        print("1. pasta = 50\n2. pastas = 100\n3. pastas = 150")
-        print("***Buy 4 or more pastas and get 2 bruschetta free.***")
-        print("***Buy 4 or more pizzas and pastas and get 2 chocco brownies ice cream free.***")
-        print("--------------------------------")
+    def menu(self):
+        print(
+            """1. Large pizza = 100\n2. Large Pizzas = 200\n3. Large Pizzas = 300
+***Buy 4 or more pizzas and get 1.5lt of soft drink free***
+1. Pasta = 50\n2. Pastas = 100\n3. Pastas = 150
+***Buy 4 or more pastas and get 2 bruschetta free.***
+***Buy 4 or more pizzas and pastas and get 2 chocco brownies ice cream free.***
+--------------------------------"""
+        )
 
-    def take_order(self):
+    def order(self):
         name = input("Enter your name: ")
-        print(f"\nWelcome, {name}")
+        print(f"Welcome, {name}")
 
-        num_pizzas = int(input("How Many Pizza Do You Want? : "))
+        num_pizzas = int(input("How many pizzas do you want? : "))
         pizza_amount = num_pizzas * 100
-        print(f"\nYour pizza order amount is : {pizza_amount}")
+        print(f"Your pizza order amount is: {pizza_amount}")
 
         if num_pizzas >= 4:
             print("***Congratulations!! 1.5lt soft drink free***")
             self.total_soft_drinks_given += 1
 
-        num_pastas = int(input("\nHow Many Pasta Do You Want? : "))
+        num_pastas = int(input("\nHow many pastas do you want? : "))
         pasta_amount = num_pastas * 50
-        print(f"\nYour pasta order amount is : {pasta_amount}")
+        print(f"Your pasta order amount is: {pasta_amount}")
 
         if num_pastas >= 4:
-            print("***Congratulations!! get 2 bruschetta free***")
+            print("***Congratulations!! Get 2 bruschetta free***")
             self.total_bruschetta_given += 2
 
         if num_pizzas >= 4 and num_pastas >= 4:
-            print("***Congratulations!! get 2 chocco brownies ice cream free***")
+            print("***Congratulations!! Get 2 chocco brownies ice cream free***")
             self.total_chocco_brownies_given += 2
 
         total_order = pizza_amount + pasta_amount
-        print(f"\nYour total order is : {total_order}")
+        print(f"Your total order is: {total_order}")
 
         self.total_order_amount += total_order
         self.total_payment_received += total_order
         self.total_pizza_sold += num_pizzas
         self.total_pasta_sold += num_pastas
 
-    def display_summary(self):
+    def collections(self):
         print("\n----------- Pizza and pasta Bill --------------")
         print(f"Payment received today: {self.total_payment_received}")
-        print(f"Number of pizza sold in one shift: {self.total_pizza_sold}")
-        print(f"Number of pasta sold in one shift: {self.total_pasta_sold}")
+        print(f"Number of pizzas sold in one shift: {self.total_pizza_sold}")
+        print(f"Number of pastas sold in one shift: {self.total_pasta_sold}")
         print(f"Number of 1.5lt soft drink bottles given: {self.total_soft_drinks_given}")
         print(f"Number of bruschetta given to customers: {self.total_bruschetta_given}")
-        print(f"Number of chocco brownies ice cream given to customers: {self.total_chocco_brownies_given}")
+        print(
+            f"Number of chocco brownies ice cream given to customers: {self.total_chocco_brownies_given}"
+        )
         print("\nBye Bye!!!")
 
 
@@ -63,12 +67,13 @@ pizza_system = PizzaOrderSystem()
 
 while True:
     print("\nWelcome to Tops Restaurant")
-    print("1. Order menu\n2. Exit")
+    print("1. Order menu")
+    print("2. Exit")
     choice = input("Enter your choice: ")
 
     if choice == "1":
         pizza_system.display_menu()
-        pizza_system.take_order()
+        pizza_system.order()
     elif choice == "2":
-        pizza_system.display_summary()
+        pizza_system.collections()
         break
