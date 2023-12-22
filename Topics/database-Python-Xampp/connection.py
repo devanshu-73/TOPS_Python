1f0dafb171b6fa753c45cbdab5ff4d4a169eca92
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-22 19:13:31 +0530

    ....

--- Topics/database-Python-Xampp/connection.py
+++ Topics/database-Python-Xampp/connection.py
@@ -1,13 +1,32 @@
 import pymysql
 
-mydb = pymysql.connect(host="localhost",user="root",passwd="")
-cursor = mydb.cursor()
-cursor.execute("create databse class")
-cursor.commit()
 
-cursor.execute()
+# Database Creation
+
+# mydb = pymysql.connect(host="localhost",user="root",passwd="")
+# cursor = mydb.cursor()
+# cursor.execute("create database class")
+# mydb.commit()
+
+
+# if Database already exists then
+
 mydb = pymysql.connect(host="localhost",user="root",passwd="",database="class")
 cursor = mydb.cursor()
-
 cursor.execute("create table if not exists class1 (id int primary key auto_increment , name varchar(20), subject varchar(20))")
-cursor.commit()
+mydb.commit()
+
+
+
+
+
+
+
+# Drop the topspython database
+# cursor.execute("DROP DATABASE IF EXISTS topspython")
+
+# Commit the changes
+# mydb.commit()
+
+# Close the connection
+# mydb.close()
