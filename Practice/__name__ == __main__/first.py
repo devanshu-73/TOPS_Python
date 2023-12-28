e6a2309679800cb8f699c76cfae272a7c21808fb
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-12-28 17:08:26 +0530

    ..

---
+++ Practice/__name__ == __main__/first.py
@@ -0,0 +1,7 @@
+import second
+print("Hello from first")
+
+
+
+if __name__ == "__main__":
+    print("First In Main")
---
+++ Practice/__name__ == __main__/second.py
@@ -0,0 +1,6 @@
+
+if __name__ == "__main__":
+    print("Hello second")
+    def sum():
+        print("Hello Sum function")     
+    sum()
