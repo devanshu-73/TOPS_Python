41be65047d45380c6ad0dfa2e1bb78256f07c76b
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-03 18:38:48 +0530

    Python..

--- Practice/pattern_2.py
+++ Practice/pattern_2.py
@@ -19,11 +19,11 @@ for i in range(n):
 #                 print(0,end=" ") 
 #     print()
 
-# n = int(input("Enter Number : "))
-# for i in range(n):
-#     for j in range(i + 1):
-#         print(1 if (i + j) % 2 == 0 else 0, end=" ")
-#     print()
+n = int(input("Enter Number : "))
+for i in range(n):
+    for j in range(i + 1):
+        print(1 if (i + j) % 2 == 0 else 0, end=" ")
+    print()
 
 # for i in range(5):
 #     for j in range(i+1):
