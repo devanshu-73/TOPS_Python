00569d2265e7a1b5bdb7a3f8a1ad675744502af1
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-03-02 18:23:52 +0530

    Python

--- Practice/python.py
+++ Practice/python.py
@@ -1,4 +1,12 @@
-a = [5,6,4,2,1,3]
-b= []
+# a.sort()  # Existing list
+# b= sorted(a)  # New list
+a = [5,6,4,2,7,1,3]
 
-for
+
+# bubble Sort
+for i in range(len(a)):
+    for j in range(0,len(a)-i-1):
+        if a[j] > a[j+1]:
+            a[j],a[j+1] = a[j+1],a[j]
+
+print(a)
