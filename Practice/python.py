a00c45b62da83191521e10796d8cfb9e6d2319f1
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-03-02 18:44:18 +0530

    Bubble Sort

--- Practice/python.py
+++ Practice/python.py
@@ -1,12 +1,16 @@
 # a.sort()  # Existing list
 # b= sorted(a)  # New list
-a = [5,6,4,2,7,1,3]
+a = [55,22,44,33,11]
 
 
 # bubble Sort
 for i in range(len(a)):
     for j in range(0,len(a)-i-1):
+        # print("a[j] , a[j+1] :",a[j],a[j+1])
+        print("Before :",a)
         if a[j] > a[j+1]:
+            # print("2) a[j] , a[j+1] :",a[j],a[j+1])
             a[j],a[j+1] = a[j+1],a[j]
+            print("After : ",a)
 
-print(a)
+print("Final :",a)
